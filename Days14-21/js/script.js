09cea8ec64ad8d9054e428332091f426da414d5d
Choi Ying Yau <65577867+ChoiYingY@users.noreply.github.com>
2021-01-11 23:14:10 -0500

    Update script.js

--- Days14-21/js/script.js
+++ Days14-21/js/script.js
@@ -2,3 +2,11 @@ function scrollToBottom() {
   document.body.scrollBottom!=0 || document.documentElement.scrollBottom!=0
   window.scrollBy(0,2000000);
 }
+function redirect(){
+	var url = '';
+	if(document.getElementById('more-proj-details-btn') !== null)
+		url = "myProj.html";
+	else if (document.getElementById('more-details-about-ME-btn') !== null)
+		url = "selfIntro.html";
+	window.location = url;
+}
